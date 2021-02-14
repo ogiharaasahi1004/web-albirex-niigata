189d6f290385cab23780250a44fe107a74ac2e35
asahi.ogihara <t19a014e@mail.cc.niigata-u.ac.jp>
2021-02-14 02:19:49 +0000

    set preparation

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,3 +1,11 @@
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
+end
+
+
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
