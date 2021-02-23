7fb94d39d8b4ca9714b1fae97624ca827c41a24c
asahi.ogihara <t19a014e@mail.cc.niigata-u.ac.jp>
2021-02-23 06:56:29 +0000

    commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    micropost = Micropost.find(params[:favorite_id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = '投稿をいいねしました。'
     redirect_to current_user
   end
 
   def destory
-    micropost = Micropost.find(params[:favorite_id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = '投稿のいいねを解除しました。'
     redirect_to current_user
-    end
+  end
 end
