12128314c3d69a1e9509b8ecdf1629fdc341dcb2
asahi.ogihara <t19a014e@mail.cc.niigata-u.ac.jp>
2021-02-24 11:55:45 +0000

    commit

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,8 +3,8 @@ class MicropostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def create
-    @micropost = current_user.microposts.build(micropost_params)
-    if @micropost.save
+    @microposts = current_user.microposts.build(micropost_params)
+    if @microposts.save
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   
   def likes
     @user = User.find(params[:id])
-    @likes = @user.likes.page(params[:page])
+    @likes = @user.like_microposts.page(params[:page])
     counts(@user)
   end
   
