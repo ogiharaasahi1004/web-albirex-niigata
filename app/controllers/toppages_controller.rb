0f53a7019f17922eab7b206c2fdd8f9c626d8f77
asahi.ogihara <t19a014e@mail.cc.niigata-u.ac.jp>
2021-04-05 02:52:46 +0000

    [Add] modify

--- app/controllers/toppages_controller.rb
+++
@@ -1,8 +0,0 @@
-class ToppagesController < ApplicationController
-  def index
-    if logged_in?
-      @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
-    end
-  end
-end
